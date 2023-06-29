1f406cf946a5947fb12787911f8e6820e2c519cf
victor helio <heliovictor180@gmail.com>
2023-06-29 19:12:15 -0300

    teste

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode () {
+  const html = document.documentElement
+  html.classList.toggle("ligth")
+
+  const img  = document.querySelector("#profile img")
+
+  if(html.classList.contains("ligth")) {
+    img.setAttribute("src", "")
+  } else {
+    img.setAttribute("src", "")
+  }
+}
